effdaf5b3a1c85cde332fb912a0dbdda25b6d9d1
baibaixy <2942583916@qq.com>
2022-07-13 09:53:21 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>黑马头条</h1>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  },
+  mounted () {},
+  computed: {},
+  methods: {}
+}
+</script>
+<style scoped></style>
